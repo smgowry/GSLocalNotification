9767cfde90fd442db0e47f30b24bdcc19ab34aa2
smgowry <sm_gowry@yahoo.com>
2015-09-16 18:10:09 -0700

    Initial Commit

---
+++ LocalNotificationExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotificationExample
+//
+//  Created by Gowrie Sammandhamoorthy on 9/16/15.
+//  Copyright (c) 2015 Gowrie Sammandhamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
